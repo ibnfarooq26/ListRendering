27ee0c61b07f40ba0277a5ccea981d31245c5fd4
ibnfarooq26 <85802553+ibnfarooq26@users.noreply.github.com>
2022-05-27 10:14:31 +0530

    List Rendering Using RecyclerView

---
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsListTests.kt
@@ -0,0 +1,10 @@
+package com.example.affirmations
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+class AffirmationsListTests {
+
+}
